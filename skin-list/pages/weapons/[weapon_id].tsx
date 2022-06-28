3ab13c33f823ebc71ce3cbfa4eb48cf0047981c5
danvoolu <mekashimu@gmail.com>
2022-06-28 23:33:04 +0900

    スキンを検索できるように

--- skin-list/pages/weapons/[weapon_id].tsx
+++ skin-list/pages/weapons/[weapon_id].tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 import useSWR from "swr";
+import SearchBar from "../../src/components/searchBar";
 import WeaponFrame from "../../src/components/weaponFrame";
 import WeaponsContainer from "../../src/components/weaponsContainer";
 import { ApiResponseWeapon, WeaponByUuid } from "../../src/models/weaponModel";
@@ -7,6 +9,7 @@ import { ApiResponseWeapon, WeaponByUuid } from "../../src/models/weaponModel";
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 const Index = () => {
+  const [searchWord, setSearchWord] = useState<string>("");
   const router = useRouter();
   const { weapon_id } = router.query;
   const { data, error } = useSWR<ApiResponseWeapon<WeaponByUuid>>(
@@ -20,17 +23,32 @@ const Index = () => {
   console.log(data);
   if (!data) return <></>;
   return (
-    <WeaponsContainer>
-      {data.weapons.data.skins.map((x) => (
-        <WeaponFrame
-          url={x.displayIcon ?? x.chromas[0].displayIcon}
-          id={x.uuid}
-          name={x.displayName}
-          key={x.uuid}
-          onClick={handleClickImg}
-        />
-      ))}
-    </WeaponsContainer>
+    <>
+      <SearchBar text={searchWord} setText={setSearchWord} />
+      <WeaponsContainer>
+        {data.weapons.data.skins
+          .filter((item) => {
+            if (searchWord.length === 0) {
+              return true;
+            } else {
+              return (
+                item.displayName
+                  .toLowerCase()
+                  .indexOf(searchWord.toLowerCase()) > -1
+              );
+            }
+          })
+          .map((x) => (
+            <WeaponFrame
+              url={x.displayIcon ?? x.chromas[0].displayIcon}
+              id={x.uuid}
+              name={x.displayName}
+              key={x.uuid}
+              onClick={handleClickImg}
+            />
+          ))}
+      </WeaponsContainer>
+    </>
   );
 };
 export default Index;
---
+++ skin-list/src/components/searchBar.tsx
@@ -0,0 +1,39 @@
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
+interface Props {
+  text: string;
+  setText: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SearchBar = ({ text, setText }: Props) => {
+  const handleChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        maxWidth: 500,
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Skins"
+        inputProps={{ "aria-label": "search google maps" }}
+        value={text}
+        onChange={handleChangeText}
+      />
+      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+export default SearchBar;
